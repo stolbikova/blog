08d0f14b3be851f3483704404c377025be3ce9ba
Alina <alina.stolbikova@gmail.com>
2024-07-30 18:06:36 +0200

    added STATICFILES_DIRS var.

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,6 +113,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
