7a3d69e40701b34dae58d66f507abf56248da466
Alina <alina.stolbikova@gmail.com>
2024-08-20 11:50:03 +0200

    Update settings.py
    changed database settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,9 +71,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoblog',
         'PASSWORD': env('DATABASE_PASSWORD'),
+        'USER': 'admin',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
